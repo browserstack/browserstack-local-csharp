e30050726d3ab7f848a12a752587ca77d59e9e0f
Aditya Hirapara <adityahirapara2016@gmail.com>
2020-10-23 13:32:29 +0530

    Update integration test to run automate session

--- BrowserStackLocal/BrowserStackLocalIntegrationTests/IntegrationTests.cs
+++ BrowserStackLocal/BrowserStackLocalIntegrationTests/IntegrationTests.cs
@@ -2,17 +2,21 @@ using NUnit.Framework;
 using System;
 using System.Diagnostics;
 using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Edge;
 using BrowserStack;
 
 namespace BrowserStackLocalIntegrationTests
 {
   public class IntegrationTests
   {
+    private static string username = Environment.GetEnvironmentVariable("BROWSERSTACK_USERNAME");
+    private static string accesskey = Environment.GetEnvironmentVariable("BROWSERSTACK_ACCESS_KEY");
+
     private List<KeyValuePair<string, string>> options = new List<KeyValuePair<string, string>>() {
-      new KeyValuePair<string, string>("key", Environment.GetEnvironmentVariable("BROWSERSTACK_ACCESS_KEY")),
-      //new KeyValuePair<string, string>("localIdentifier", "identifier"),
-      //new KeyValuePair<string, string>("f", "C:\\Users\\Admin\\Desktop\\"),
-      //new KeyValuePair<string, string>("onlyAutomate", "true"),
+      new KeyValuePair<string, string>("key", accesskey),
+      new KeyValuePair<string, string>("onlyAutomate", "true"),
       new KeyValuePair<string, string>("verbose", "true"),
       new KeyValuePair<string, string>("forcelocal", "true"),
       new KeyValuePair<string, string>("binarypath", "C:\\Users\\Admin\\Desktop\\BrowserStackLocal.exe"),
@@ -20,7 +24,7 @@ namespace BrowserStackLocalIntegrationTests
     };
 
     [Test]
-    public void TestStartsAndStopsLocalBinary()
+    public void TestStartsAndStopsLocalSession()
     {
       Local local = new Local();
 
@@ -33,7 +37,24 @@ namespace BrowserStackLocalIntegrationTests
       Process[] binaryInstances = Process.GetProcessesByName("BrowserStackLocal");
       Assert.AreNotEqual(binaryInstances.Length, 0);
 
+      IWebDriver driver;
+      EdgeOptions capability = new EdgeOptions();
+      capability.AddAdditionalCapability("browserstack.user", username);
+      capability.AddAdditionalCapability("browserstack.key", accesskey);
+      capability.AddAdditionalCapability("browserstack.local", true);
+      capability.AddAdditionalCapability("build", "C Sharp binding Integration Test");
+
+      driver = new RemoteWebDriver(
+        new Uri("http://hub.browserstack.com/wd/hub/"), capability
+      );
+      driver.Navigate().GoToUrl("http://bs-local.com:45691/check");
+      String status = driver.FindElement(By.TagName("body")).Text;
+
+      Assert.AreEqual(status, "Up and running");
+
+      driver.Quit();
       local.stop();
+
       binaryInstances = Process.GetProcessesByName("BrowserStackLocal");
       Assert.AreEqual(binaryInstances.Length, 0);
     }
